5e05c6249de22390136963185b2e431684d6baff
K Sutulova <yunusova.kk@inbox.ru>
2024-12-17 20:45:40 +0300

    first commit

---
+++ my-app/src/app/list-component/list-component.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-component',
+  templateUrl: './list-component.component.html',
+  styleUrls: ['./list-component.component.scss']
+})
+export class ListComponentComponent implements OnInit {
+  items = [
+    { id: 1, name: 'Item 1', selected: false },
+    { id: 2, name: 'Item 2', selected: false },
+    { id: 3, name: 'Item 3', selected: true }
+  ];
+
+  onSelect(item: any) {
+    item.selected = !item.selected;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
