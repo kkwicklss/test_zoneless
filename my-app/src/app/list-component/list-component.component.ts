675fcffc2918e58ce57742fb00608ac38f388a80
K Sutulova <yunusova.kk@inbox.ru>
2024-12-18 17:27:27 +0300

    List added

--- my-app/src/app/list-component/list-component.component.ts
+++ my-app/src/app/list-component/list-component.component.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Option {
+  label: string;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-list-component',
   templateUrl: './list-component.component.html',
   styleUrls: ['./list-component.component.scss']
 })
 export class ListComponentComponent implements OnInit {
+  showDropdown = false;
+  showSteps = false;
+  // showDropdown = true;
+  // showSteps = true;
   items = [
-    { id: 1, name: 'Item 1', selected: false },
-    { id: 2, name: 'Item 2', selected: false },
-    { id: 3, name: 'Item 3', selected: true }
+    { id: 1, name: 'Продажи', selected: false },
+    { id: 2, name: 'Сотрудники', selected: false },
+    { id: 3, name: 'Партнёры', selected: false },
+    { id: 4, name: 'Ивент', selected: false },
+    { id: 5, name: 'Входящие обращения', selected: false }
   ];
+  steps = [
+    { id: 1, name: 'Неразобранное', color: '#99ccfd', selected: true },
+    { id: 2, name: 'Переговоры', color: '#ffff99', 
+    selected: false },
+    { id: 3, name: 'Принимают решение', 
+    color: '#ffcc66',
+    selected: false },
+    { id: 4, name: 'Успешно', 
+    color: '#ccff66', 
+    selected: false }
+  ]
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  toggleSteps() {
+    this.showSteps = !this.showSteps;
+  }
 
   onSelect(item: any) {
     item.selected = !item.selected;
